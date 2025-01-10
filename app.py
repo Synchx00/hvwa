f16a47a816489cb029d62acdddcd9a13f6627384
Synchx <hatta.asnan00@gmail.com>
2025-01-10 15:58:49 +0700

    nginx server successfully run

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p>Hello, World!</p>"
+    return render_template('page.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
